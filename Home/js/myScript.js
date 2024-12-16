59fd15f717deb05c6c81c1a781484475169250a4
Robert Philippe Gerong <philippe.gerong@gmail.com>
2024-12-16 23:54:24 +0800

    yeah

--- Home/js/myScript.js
+++ Home/js/myScript.js
@@ -1,3 +1,10 @@
 function myFunction() {
-    document.getElementById("demo").innerHTML = "Paragraph changed.";
-  }
+  document.getElementById("demo").innerHTML = "I am Philippe";
+}
+
+function myFunction2() {
+  const element = document.getElementsByTagName("a");
+
+  document.getElementById("demo").innerHTML = 'Wait for it ' + element[0].innerHTML;
+
+}
