633ae6a603c1cb0d4edfa0a2b32e153027d4da8f
Robert Philippe Gerong <philippe.gerong@gmail.com>
2025-01-16 16:12:40 +0800

    added carousel script

--- Home/js/script.js
+++ Home/js/script.js
@@ -1,5 +1,17 @@
+let currentSlide = 0;
+
 function showContent(id) {
     const contents = document.querySelectorAll('.content');
     contents.forEach(content => content.classList.add('hidden'));
     document.getElementById(id).classList.remove('hidden');
+}
+
+function moveSlide(direction) {
+    const images = document.querySelectorAll('.carousel img');
+    const totalSlides = images.length;
+    
+    currentSlide = (currentSlide + direction + totalSlides) % totalSlides;
+    
+    const carouselImages = document.querySelector('.carousel-images');
+    carouselImages.style.transform = `translateX(-${currentSlide * 100}%)`;
 }
